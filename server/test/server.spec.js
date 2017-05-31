09f2adf70d18a869fdb80064a8e9af92839ef5ed
kale007 <rajas.p.kale@gmail.com>
2017-05-31 16:28:22 -0700

    (test) creates server integration tests with database to fetch seed data

--- server/test/server.spec.js
+++ server/test/server.spec.js
@@ -4,24 +4,38 @@ const express = require('express');
 const expect = require('chai').expect;
 const app = require('../app.js');
 
-describe('basic server', function() {
-  it('sends back hello world', function(done) {
+describe('fetches data: ', function () {
+  it('does not fetch from unknown endpoints', function (done) {
     request(app)
-      .get('/api')
+      .get('/arglebargle')
+      .expect(404)
+      .end(done);
+  })
+
+  it('fetches all data in users table', function (done) {
+    request(app)
+      .get('/users')
       .expect(200)
       .expect(function(res) {
-        expect(res.text).to.equal('Hello World!');
+        expect(res.body[0].full_name).to.equal('Rajas Kale');
+        expect(res.body[1].full_name).to.equal('Alan Zheng');
+        expect(res.body[2].full_name).to.equal('Lavanya AC');
+        expect(res.body[0].summary).to.equal('I like turtles');
+        expect(res.body[0].full_name).to.not.equal('Argle Bargle');
       })
       .end(done);
-  });
+  })
 
-  it('accepts POST request', function(done) {
+  it('fetches all data in experience table', function (done) {
     request(app)
-      .post('/api')
-      .expect(201)
+      .get('/experience')
+      .expect(200)
       .expect(function(res) {
-        expect(res.body.data).to.equal('Posted!');
+        expect(res.body[0].role).to.equal('PhD student');
+        expect(res.body[3].role).to.equal('Finance & Operations Analyst');
+        expect(res.body[6].role).to.equal('Software Engineer');
+        expect(res.body[0].organization).to.not.equal('I like turtles');
       })
       .end(done);
-  });
-});
+  })
+})
