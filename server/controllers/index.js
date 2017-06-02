a16e7c204123750e69dda3185f19709daec5aad5
kale007 <rajas.p.kale@gmail.com>
2017-06-01 18:22:28 -0700

    (server) builds routing for connection insertions

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,4 @@
-module.exports.Profiles = require('./profiles');
+// module.exports.Profiles = require('./profiles');
 module.exports.Users = require('./users');
 module.exports.Experience = require('./experience');
+module.exports.Connection = require('./connection');
