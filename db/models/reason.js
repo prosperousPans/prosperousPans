f66d4f2f8677c9a45e7f78dd399c8b5660628b50
kale007 <rajas.p.kale@gmail.com>
2017-06-07 10:43:09 -0700

    seeds new users, creates separate connections seeds file, updates schemas and models to remove reasons tables

--- db/models/reason.js
+++ db/models/reason.js
@@ -1,10 +1,10 @@
-const db = require('../');
-
-const Reason = db.Model.extend({
-  tableName: 'reason',
-  connection_reason: function() {
-    return this.hasMany('connection_reason');
-  }
-});
-
-module.exports = db.model('Reason', Reason);
+// const db = require('../');
+//
+// const Reason = db.Model.extend({
+//   tableName: 'reason',
+//   connection_reason: function() {
+//     return this.hasMany('connection_reason');
+//   }
+// });
+//
+// module.exports = db.model('Reason', Reason);
