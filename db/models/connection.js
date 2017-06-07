0887191bbbc2124cf44dac2f5483f527f1feab87
kale007 <rajas.p.kale@gmail.com>
2017-06-07 10:52:03 -0700

    updates connection table to seed last

--- db/models/connection.js
+++ db/models/connection.js
@@ -4,10 +4,10 @@ const Connection = db.Model.extend({
   tableName: 'connection',
   users: function() {
     return this.belongsTo('users');
-  },
-  connection_reason: function() {
-    return this.hasMany('connection_reason');
   }
+  // connection_reason: function() {
+  //   return this.hasMany('connection_reason');
+  // }
 });
 
 module.exports = db.model('Connection', Connection);
